dcc943966ec62b4cf39f94b5d745544f0e13cb92
Bro <38715190+Wirevel@users.noreply.github.com>
2019-06-20 10:05:41 +0300

    Update.1

---
+++ src/kesäharjoitus1/Bensa.java
@@ -0,0 +1,15 @@
+package kesäharjoitus1;
+
+public class Bensa {
+	static double kulutus=7.5;
+	static double hinta=1.582;
+	static int matka=300;
+	static double BensanKulutus=(kulutus*matka)/100;
+	static double matkanHinta=hinta*BensanKulutus;
+
+	public static void main(String[] args) {
+		System.out.println("Bensan kulutus on "+BensanKulutus+" litraa.");
+		System.out.println("Matkalle tulee hintaa "+matkanHinta+" euroa.");
+	}
+
+}
